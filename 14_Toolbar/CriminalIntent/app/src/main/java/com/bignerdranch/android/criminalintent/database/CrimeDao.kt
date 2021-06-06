9c66102c0a048e02449c7b57a68b72796c17a561
sby5388PC <sby5388@github.com>
2021-06-06 20:27:40 +0800

    add chap14 Toolbar CriminalIntent

---
+++ 14_Toolbar/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.criminalintent.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.bignerdranch.android.criminalintent.Crime
+import java.util.*
+
+@Dao
+interface CrimeDao {
+
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): LiveData<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): LiveData<Crime?>
+
+    @Update
+    fun updateCrime(crime: Crime)
+
+    @Insert
+    fun addCrime(crime: Crime)
+}
