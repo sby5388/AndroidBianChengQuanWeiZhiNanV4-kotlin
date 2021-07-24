13504461a5a4ab81a727c34e54596ed565505199
sby5388 <44052699+sby5388@users.noreply.github.com>
2021-07-24 23:44:44 +0800

    add chap30 TouchEvent DragAndDraw
    自定义View；
    触摸事件，响应绘制；

---
+++ 30_TouchEvents/DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/Box.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.draganddraw
+
+import android.graphics.PointF
+
+class Box(val start: PointF) {
+
+    var end: PointF = start
+
+    val left: Float
+        get() = Math.min(start.x, end.x)
+
+    val right: Float
+        get() = Math.max(start.x, end.x)
+
+    val top: Float
+        get() = Math.min(start.y, end.y)
+
+    val bottom: Float
+        get() = Math.max(start.y, end.y)
+}
