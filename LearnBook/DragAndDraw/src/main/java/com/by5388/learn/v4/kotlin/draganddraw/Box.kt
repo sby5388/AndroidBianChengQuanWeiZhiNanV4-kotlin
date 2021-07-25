8fec0bda3b476b42911f098d9e110a4523001b07
sby5388 <44052699+sby5388@users.noreply.github.com>
2021-07-25 21:53:03 +0800

    learn 30.6 when screen rotate CustomView save data
    添加30.6 挑战练习：设备旋转问题
    通过onSaveInstanceState 和 onRestoreInstanceState 保存数据和恢复数据
    参考自：https://blog.csdn.net/artzok/article/details/50172657

--- LearnBook/DragAndDraw/src/main/java/com/by5388/learn/v4/kotlin/draganddraw/Box.kt
+++ LearnBook/DragAndDraw/src/main/java/com/by5388/learn/v4/kotlin/draganddraw/Box.kt
@@ -1,19 +1,64 @@
 package com.by5388.learn.v4.kotlin.draganddraw
 
 import android.graphics.PointF
+import android.os.Parcel
+import android.os.Parcelable
 
-class Box(private val start: PointF) {
-    var end: PointF = start
+class Box() : Parcelable {
+
+    private lateinit var start: PointF
+    lateinit var end: PointF
+
+    constructor(s: PointF) : this() {
+        start = s
+        end = s
+    }
+
+    constructor(`in`: Parcel) : this() {
+        start = PointF()
+        end = PointF()
+        start.x = `in`.readFloat()
+        start.y = `in`.readFloat()
+        end.x = `in`.readFloat()
+        end.y = `in`.readFloat()
+    }
+
     //Math.min(start.x, end.x)
     val left: Float
         get() = start.x.coerceAtMost(end.x)
+
     //Math.max(start.x, end.x)
     val right: Float
         get() = start.x.coerceAtLeast(end.x)
+
     //min
     val top: Float
         get() = start.y.coerceAtMost(end.y)
+
     //max
     val bottom: Float
         get() = start.y.coerceAtLeast(end.y)
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeFloat(start.x)
+        dest.writeFloat(start.y)
+        dest.writeFloat(end.x)
+        dest.writeFloat(end.y)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Box> {
+        override fun createFromParcel(parcel: Parcel): Box {
+            return Box(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Box?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+
 }
--- LearnBook/DragAndDraw/src/main/java/com/by5388/learn/v4/kotlin/draganddraw/BoxDrawingView.kt
+++ LearnBook/DragAndDraw/src/main/java/com/by5388/learn/v4/kotlin/draganddraw/BoxDrawingView.kt
@@ -4,12 +4,18 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import java.util.*
 
 private const val TAG = "BoxDrawingView"
+private const val KEY_SUPER_DATA = "super_data"
+private const val KEY_BOX = "box"
+private const val KEY_BOX_LIST = "boxList"
 
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
     private var mCurrentBox: Box? = null
@@ -54,13 +60,45 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
         super.onDraw(canvas)
         canvas.drawPaint(mBgPaint)
         mBoxList.forEach { box ->
-            canvas.drawRect(box.left, box.top, box. right, box.bottom, mBoxPaint)
+            canvas.drawRect(box.left, box.top, box.right, box.bottom, mBoxPaint)
         }
     }
 
-    private fun updateCurrentBox(currenct: PointF) {
+    /**
+     * 需要View设置了ID:保存数据
+     * https://blog.csdn.net/artzok/article/details/50172657
+     */
+    override fun onSaveInstanceState(): Parcelable? {
+        val superState = super.onSaveInstanceState()
+        val bundle = Bundle()
+        bundle.putParcelable(KEY_SUPER_DATA, superState)
+        bundle.putParcelable(KEY_BOX, mCurrentBox)
+        bundle.putParcelableArrayList(KEY_BOX_LIST, mBoxList as ArrayList<Box>)
+        return bundle
+    }
+
+    /**
+     * 需要View设置了ID：
+     * https://blog.csdn.net/artzok/article/details/50172657
+     */
+    override fun onRestoreInstanceState(state: Parcelable) {
+        val bundle = state as Bundle
+        val superData = bundle.getParcelable<Parcelable>(KEY_SUPER_DATA)
+        mCurrentBox = bundle.getParcelable(KEY_BOX)
+        val boxList = bundle.getParcelableArrayList<Box>(KEY_BOX_LIST)
+        boxList?.let {
+            mBoxList.clear()
+            mBoxList.addAll(boxList)
+        }
+        // TODO: 2021/7/25 不能将原始数据传递给父类，否则会奔溃
+        super.onRestoreInstanceState(superData)
+
+    }
+
+
+    private fun updateCurrentBox(current: PointF) {
         mCurrentBox?.let {
-            it.end = currenct
+            it.end = current
             invalidate()
         }
     }
